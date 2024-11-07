7fbb1cb8416ad0ebd1613cff5d32d7b72ac8cc03
Maurice <info@heymann.dev>
2024-11-07 21:57:04 +0100

    added iso support

---
+++ nixos/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  hostname,
+  isInstall,
+  isWorkstation,
+  inputs,
+  lib,
+  modulesPath,
+  outputs,
+  pkgs,
+  platform,
+  stateVersion,
+  username,
+  ...
+}:
+{
+  imports = [];
+}
