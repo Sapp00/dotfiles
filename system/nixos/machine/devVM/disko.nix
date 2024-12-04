f1d836046ce4dd3a624b6779d35df5980af82c07
Maurice Heymann <info@heymann.dev>
2024-12-04 20:01:22 +0100

    changed disko layout

--- system/nixos/machine/devVM/disko.nix
+++ system/nixos/machine/devVM/disko.nix
@@ -1,21 +1,25 @@
-{ self
-, lib
-, hostname
-, pkgs
-, ...
-}:
 {
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/sda";
+        # When using disko-install, we will overwrite this value from the commandline
+        device = "/dev/disk/by-id/some-disk-id";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
+            MBR = {
               type = "EF02"; # for grub MBR
+              size = "1M";
+            };
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
             root = {
               size = "100%";
