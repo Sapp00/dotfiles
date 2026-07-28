e27e56c82ec2c331d5cf470ec59e46f6f28e410e
Maurice Heymann <info@heymann.dev>
2026-07-28 09:53:31 +0200

    added hyprland configuration

--- home-manager/desktops/hyprland/default.nix
+++ home-manager/desktops/hyprland/default.nix
@@ -1,9 +1,81 @@
 { isWorkstation
 , isLinux
+, pkgs
 , ...
 }:
 {
   module = {
     kitty.enable = true;
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+      "$mod" = "SUPER";
+
+      bind = [
+        # Applications
+        "$mod, Return, exec, kitty"
+        "$mod, Q, killactive"
+        "$mod, F, fullscreen"
+        "$mod, V, togglefloating"
+        "$mod, M, exit"
+
+        # Focus (hjkl)
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
+
+        # Move windows (hjkl)
+        "$mod SHIFT, h, movewindow, l"
+        "$mod SHIFT, l, movewindow, r"
+        "$mod SHIFT, k, movewindow, u"
+        "$mod SHIFT, j, movewindow, d"
+
+        # Workspaces
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        # Move window to workspace
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        # Scroll through workspaces
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+      ];
+
+      # Resize windows with SUPER + CTRL + hjkl (like tmux pane resize)
+      binde = [
+        "$mod CTRL, h, resizeactive, -40 0"
+        "$mod CTRL, l, resizeactive, 40 0"
+        "$mod CTRL, k, resizeactive, 0 -40"
+        "$mod CTRL, j, resizeactive, 0 40"
+      ];
+
+      # Mouse bindings
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+    };
   };
 }
