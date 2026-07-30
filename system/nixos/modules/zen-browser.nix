d519cdb15cdab17178db33d07f805c69c9ebc4a5
Maurice Heymann <info@heymann.dev>
2026-07-30 11:17:56 +0200

    fix Zen Browser: use wrapFirefox with unwrapped package for policies and extensions

--- system/nixos/modules/zen-browser.nix
+++ system/nixos/modules/zen-browser.nix
@@ -3,23 +3,39 @@
 let
   system = pkgs.stdenv.hostPlatform.system;
 
-  zen-browser = inputs.zen-browser.packages.${system}.default.override {
-    extraPolicies = {
-      SearchEngines = {
-        Default = "DuckDuckGo";
-      };
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          installation_mode = "force_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        };
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          installation_mode = "force_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+  extension = shortId: guid: {
+    name = guid;
+    value = {
+      install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+      installation_mode = "normal_installed";
+    };
+  };
+
+  extensions = [
+    (extension "ublock-origin" "uBlock0@raymondhill.net")
+    (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+  ];
+
+  zen-browser = pkgs.wrapFirefox
+    inputs.zen-browser.packages.${system}.zen-browser-unwrapped
+    {
+      extraPrefs = lib.concatLines (
+        lib.mapAttrsToList (
+          name: value: ''lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});''
+        ) {
+          "extensions.autoDisableScopes" = 0;
+          "extensions.pocket.enabled" = false;
+        }
+      );
+
+      extraPolicies = {
+        DisableTelemetry = true;
+        ExtensionSettings = builtins.listToAttrs extensions;
+        SearchEngines = {
+          Default = "ddg";
         };
       };
     };
-  };
 in
 
 lib.mkIf (isWorkstation && pkgs.stdenv.isLinux) {
